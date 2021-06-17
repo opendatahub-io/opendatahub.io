c21ec0fa113889767077cfce4c9235cfc2ef7cca
Lucas Fernandez <lferrnan@redhat.com>
2021-06-17 18:24:31 +0200

    Updated landscape and tweaked config

---
+++ landscape/_next/static/itl7MCzFMLSYG2Ld3BOOs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fembed","\u002Facquisitions","\u002Ffullscreen\u002F[[...landscape]]","\u002Fpages\u002F[page]","\u002F[[...landscape]]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ landscape/_next/static/n8yUH-VahZEMJzNUk1SDi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fembed","\u002Facquisitions","\u002Ffullscreen\u002F[[...landscape]]","\u002F[[...landscape]]","\u002Fpages\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
