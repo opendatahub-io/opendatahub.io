75491867ac324cab141528f95701fe46d5d4234d
Gage Krumbach <gkrumbach@gmail.com>
2023-04-18 15:05:29 -0500

    added navbar, rss feed, fixed ts config (#94)
    * added navbar, rss feed, fixed ts config
    
    * moved nav to layout
    
    * Navbar uses Button Link combo now

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,62 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     `gatsby-plugin-sass`,
-    "gatsby-plugin-google-gtag",
+    // "gatsby-plugin-google-gtag", // TODO: set up google analytics
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields { 
+                      slug 
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Open Data Hub RSS Feed",
+            match: "^/blog/",
+
+          },
+        ],
+      },
+    },
      {
       resolve: "gatsby-transformer-remark",
       options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import { Header, CompetitiveAdvantage, Personas, CaseStudies, ArchitectureMap, CommunityDetails, FeaturedBlogs } from "../components/pages/home"
-import { Footer, Layout, Navbar } from "../components/shared"
+import { Footer, Layout } from "../components/shared"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout location='/'>
-      <Navbar />
       <Header />
       <CompetitiveAdvantage />
       <Personas />
