a565593feb0bd4d02c44cdeb9b13adbccc4a7b8c
Gage Krumbach <gkrumbach@gmail.com>
2023-05-21 22:23:41 -0500

    added persona content and arch content

--- src/components/pages/home/PersonasLayout/PersonasLayout.tsx
+++ src/components/pages/home/PersonasLayout/PersonasLayout.tsx
@@ -9,226 +9,14 @@ import {
 } from "@patternfly/react-core";
 import React from "react";
 
-import logo from "../../../../content/assets/img/logos/datahub_mark_color-blkbg.png";
-import placeholderImage from "../../../../content/assets/img/placeholder.svg";
-import { PlainImageCard, ContentCardList, PlainImageCardProps } from "../../../shared";
-import { FeatureCard, FeatureCardProps } from "./FeatureCard";
+import { PlainImageCard, ContentCardList } from "../../../shared";
+import { FeatureCard } from "./FeatureCard";
 import "./PersonasLayout.css";
+import { PERSONAS } from "../../../../const";
 
-type PersonaBenefits = {
-  name: string;
-  benefits: (PlainImageCardProps & {
-    features: FeatureCardProps[];
-  })[];
-}[];
-const data: PersonaBenefits = [
-  {
-    name: "Data Scientist",
-    benefits: [
-      {
-        title: "Jupyter Notebooks",
-        body: "Create and train models in Jupyter notebooks",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "External Data Sources",
-            body: "Connect to external data sources (Amazon S3)",
-            imageUrl: logo,
-          },
-          {
-            title: "Project Organization",
-            body: "Use projects to organize your work and share with colleagues",
-            imageUrl: logo,
-          },
-          {
-            title: "Experimentation",
-            body: "Run pipelines to experiment with your models",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Model Serving",
-        body: "Serve models and create endpoints for application usage",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Feature 1 Title",
-            body: "Feature 1 Body",
-            imageUrl: logo,
-          },
-          {
-            title: "Feature 2 Title",
-            body: "Feature 2 Body",
-            imageUrl: logo,
-          },
-          {
-            title: "Feature 3 Title",
-            body: "Feature 3 Body",
-            imageUrl: logo,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "IT Operator",
-    benefits: [
-      {
-        title: "Customization",
-        body: "Customize ODH: notebooks and model servers sizes, show/hide specific components, add tile to the launcher, import custom images, etc.",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Custom Notebook Images",
-            body: "Create custom notebook images so your users can start with the right software installed",
-            imageUrl: logo,
-          },
-          {
-            title: "Resource Access Control",
-            body: "Use OpenShift's built-in capabilities to control what resources your users can access",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Troubleshooting",
-        body: "Troubleshoot ODH: notebooks won't launch, how to interpret error messages",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Taints for Resource Access Restriction",
-            body: "Play with taints to restrict access to specific resources",
-            imageUrl: logo,
-          },
-          {
-            title: "Feature 2",
-            body: "Feature Body 2",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Deployment of Custom Model Server Engines",
-        body: "Deploy custom Model Server engines (Inference servers)",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Idle Notebook Management",
-            body: "Manage idle notebooks so you're not paying for resources you don't need to",
-            imageUrl: logo,
-          },
-          {
-            title: "Scaling Model Server Instances",
-            body: "Scale the deployment of custom Model Server engines to handle increased workload",
-            imageUrl: logo,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "App Developer",
-    benefits: [
-      {
-        title: "Collaborative Model Development",
-        body: "Get access to models your data science colleagues are working on so you can work collaboratively with them",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Shared Model Access",
-            body: "Collaborate with data science colleagues by accessing their models",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Jupyter Notebook Integration",
-        body: "Open existing models in Jupyter Notebooks to ensure they include the information you need in your application",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Model Integration in Notebooks",
-            body: "Seamlessly integrate existing models in Jupyter Notebooks",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Model Endpoint Management",
-        body: "Access and create multiple model endpoints",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Endpoint Creation and Management",
-            body: "Efficiently manage and create multiple model endpoints",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Unified User Interface",
-        body: "Use either the built-in ODH UI or see all the same resources in the familiar OpenShift console",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "User-Friendly Interface",
-            body: "Access resources through an intuitive user interface",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Model Containerization",
-        body: "Containerize models and serve them manually",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "Manual Model Server Deployment",
-            body: "Containerize and serve models manually",
-            imageUrl: logo,
-          },
-          {
-            title: "LLM Model Serving",
-            body: "Serve LLM models from the Model Server",
-            imageUrl: logo,
-          },
-        ],
-      },
-      {
-        title: "Granular Pipeline Control",
-        body: "Manually create/modify Pipelines with YAMLs to have more granular control and integrate them into a proper GitOps process",
-        link: "/docs/",
-        imageUrl: placeholderImage,
-        features: [
-          {
-            title: "YAML-based Pipeline Management",
-            body: "Create and modify Pipelines with YAML for fine-grained control",
-            imageUrl: logo,
-          },
-          {
-            title: "GitOps Integration",
-            body: "Integrate Pipelines into a GitOps process for better workflow management",
-            imageUrl: logo,
-          },
-        ],
-      },
-    ],
-  }
-];
 
 export const PersonasLayout = () => {
-  const [isSelected, setIsSelected] = React.useState(data[0].name);
+  const [isSelected, setIsSelected] = React.useState(PERSONAS[0].name);
   const handleItemClick = (
     isSelected: boolean,
     event: React.MouseEvent<any> | React.KeyboardEvent | MouseEvent
@@ -255,7 +43,7 @@ export const PersonasLayout = () => {
           </FlexItem>
           <FlexItem>
             <ToggleGroup>
-              {data.map((persona) => (
+              {PERSONAS.map((persona) => (
                 <ToggleGroupItem
                   key={persona.name}
                   text={persona.name}
@@ -268,7 +56,7 @@ export const PersonasLayout = () => {
           </FlexItem>
         </Flex>
       </StackItem>
-      {data.map(
+      {PERSONAS.map(
         (persona) =>
           isSelected === persona.name && (
             <StackItem key={persona.name}>
--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,6 @@
-import { LinkedContent, SideNavItemConfig } from "./types";
+import { LinkedContent, PersonaBenefits, SideNavItemConfig } from "./types";
+import logo from "./content/assets/img/logos/datahub_mark_color-blkbg.png";
+import placeholderImage from "./content/assets/img/placeholder.svg";
 
 export const DOCS_NAVIGATION: SideNavItemConfig[] = [
     {
@@ -213,8 +215,191 @@ const LINKED_CONTENT: LinkedContent[] = [
 ]
 
 export enum SourceInstanceNameLabel {
-    videos="VIDEO",
-    blog="BLOG",
-    audio="AUDIO",
-    releases="RELEASE"
+    videos = "VIDEO",
+    blog = "BLOG",
+    audio = "AUDIO",
+    releases = "RELEASE"
 }
+
+export const PERSONAS: PersonaBenefits = [
+    {
+        name: "IT Operator",
+        benefits: [
+            {
+                title: "Effortless Deployment",
+                body: "Deploy the Open Data Hub (ODH) AI platform within minutes, enabling swift provisioning and setup by IT operators. ODH offers an accelerated path to accessing AI/ML tools for data scientists.",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "Streamlined Integration",
+                        body: "Integrate seamlessly with popular open-source AI/ML tools, providing a unified user experience for data scientists.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Rapid Deployment of Resources",
+                        body: "Decrease the time it takes to deploy the necessary resources for data scientists, enabling them to start working quickly and efficiently.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+            {
+                title: "Democratized Access",
+                body: "Democratize access to open-source AI/ML tools by making them easily available to data scientists. ODH empowers IT operators to meet the increasing demand for these tools.",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "Unified User Experience",
+                        body: "Provide data scientists with a unified user experience across a range of popular open-source AI/ML tools, facilitating seamless collaboration and productivity.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Open-Source Integration",
+                        body: "Integrate with a diverse range of open-source AI/ML tools, allowing data scientists to leverage the best tools for their specific needs.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+            {
+                title: "Customization",
+                body: "Effortlessly customize the Open Data Hub (ODH) to meet specific requirements, such as adjusting notebook and model server sizes, hiding or displaying specific components, adding custom tiles to the launcher, importing custom images, and more.",
+                link: "/docs/",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "Custom Notebook Images",
+                        body: "Create and deploy custom notebook images, empowering users to start their work with the necessary software already installed.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Resource Access Control",
+                        body: "Leverage OpenShift's built-in capabilities to precisely control users' access to resources, ensuring optimal security and privacy measures.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Taints for Resource Access Restriction",
+                        body: "Effectively utilize taints to restrict access to specific resources within the ODH environment, enhancing security and resource management.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: "Data Scientist",
+        benefits: [
+            {
+                title: "Jupyter Notebooks",
+                body: "Utilize Jupyter notebooks to create and train machine learning models.",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "R Studio Support",
+                        body: "Leverage R Studio for creating and training machine learning models.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Open Source AI Tools",
+                        body: "Utilize a wide range of open source tools for data science, including TensorFlow, PyTorch, and more.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+            {
+                title: "Model Serving",
+                body: "Serve trained models and create endpoints for seamless integration into applications.",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "Repeatable Experiments",
+                        body: "Implement data science pipelines using Kubeflow Pipelines and Tekton to create repeatable experiments for consistent and reliable results.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Model Explainability",
+                        body: "Enhance model interpretability and gain insights into the factors driving predictions using TrustyAI.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Streamlined Deployment",
+                        body: "Effortlessly deploy machine learning models in production environments, including popular cloud-based platforms.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+            {
+                title: "Collaboration and Connectivity",
+                body: "Collaborate with colleagues to share models and work together on projects.",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "Shared Model Access",
+                        body: "Collaborate with colleagues by accessing their models and sharing your own.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Efficient Project Organization",
+                        body: "Leverage project management capabilities to efficiently organize your work and collaborate with colleagues.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Access External Data Sources",
+                        body: "Connect seamlessly to external data sources like Amazon S3 for comprehensive data exploration and analysis.",
+                        imageUrl: logo,
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        name: "App Developer",
+        benefits: [
+            {
+                title: "Enhanced Development Experience",
+                body: "Improve your development experience with seamless VS Code integration and version control.",
+                link: "/docs/",
+                imageUrl: placeholderImage,
+                features: [
+                    {
+                        title: "VS Code Support",
+                        body: "Enjoy seamless integration with Visual Studio Code (VS Code), a powerful development environment for building your applications.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Endpoint Creation and Management",
+                        body: "Efficiently create and manage multiple model endpoints to serve your application needs.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Manual Model Server Deployment",
+                        body: "Containerize and serve models manually",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+            {
+                title: "Streamlined Workflow Management",
+                body: "Improve your application development workflow with granular pipeline control and GitOps integration.",
+                link: "/docs/",
+                imageUrl: placeholderImage,
+                features: [
+
+                    {
+                        title: "CI/CD Integration",
+                        body: "Leverage CI/CD tools to streamline your application development lifecycle and ensure efficient deployment processes.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "GitOps Integration",
+                        body: "Integrate Pipelines into a GitOps process to streamline application deployment to stage and production environments.",
+                        imageUrl: logo,
+                    },
+                    {
+                        title: "Application and Model Versioning",
+                        body: "Efficiently manage versions of your applications and models for better organization and tracking.",
+                        imageUrl: logo,
+                    },
+                ],
+            },
+        ],
+    }
+];
