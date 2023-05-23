7b92da6dbd1c4809b6c13fde33ffc1ba69fdb87c
Gage Krumbach <gkrumbach@gmail.com>
2023-05-23 17:12:51 -0500

    update mailing list in other locations

--- src/components/pages/community/MailingList.tsx
+++ src/components/pages/community/MailingList.tsx
@@ -7,7 +7,7 @@ export const MailingList: React.FC = () => {
       title="Join community mailing list"
       description="Stay up to date with the latest announcements and discussion about the Open Data Hub."
       actionTitle="Join mailing list"
-      actionLink="https://lists.opendatahub.io/admin/lists/"
+      actionLink="https://lists.opendatahub.io/admin/lists/announcements.lists.opendatahub.io/"
       buttonProps={{ type: "button", variant: "primary", isLarge: true }}
     />
   )
