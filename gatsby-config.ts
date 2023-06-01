c966114ef1b594f56f5bd0b6fc323984c5932a8f
Gage Krumbach <gkrumbach@gmail.com>
2023-06-01 15:47:43 -0500

    updated google tag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -64,7 +64,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: process.env.REACT_APP_GOOGLE_ANALYTICS_TAG_IDS?.split(",") ?? [],
+        trackingIds: [process.env.REACT_APP_GOOGLE_ANALYTICS_TAG_ID]
       },
     },
   ],
