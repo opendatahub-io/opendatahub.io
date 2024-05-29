b33461a308519ed46320a5feed62a38c8de389f0
aduquett <aduquett@redhat.com>
2024-05-29 09:52:48 -0400

    Point to documentation for 2.9.1 release

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -70,7 +70,7 @@ const config: GatsbyConfig = {
       options: {
         name: `docs`,
         remote: `https://github.com/opendatahub-io/opendatahub-documentation.git`,
-        branch: `v2.9`,
+        branch: `v2.9.1`,
         local: "public/static/docs",
       },
     },
