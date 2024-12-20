c58c93a4e188840258879e77cc9eafbc1987b230
grainnejenningsRH <gjenning@redhat.com>
2024-12-20 11:20:57 +0000

    Point to docs changes for 2.16 release.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -70,7 +70,7 @@ const config: GatsbyConfig = {
       options: {
         name: `docs`,
         remote: `https://github.com/opendatahub-io/opendatahub-documentation.git`,
-        branch: `v2.15`,
+        branch: `v2.16`,
         local: "public/static/docs",
       },
     },
