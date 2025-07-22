d2078a1d9efa8ee6e994d0a0e0e1d5940d0b46ea
Chris Tyler <chtyler@redhat.com>
2025-07-22 19:59:41 +0100

    Point to documentation for 2.22 release

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -70,7 +70,7 @@ const config: GatsbyConfig = {
       options: {
         name: `docs`,
         remote: `https://github.com/opendatahub-io/opendatahub-documentation.git`,
-        branch: `v2.21`,
+        branch: `v2.22`,
         local: "public/static/docs",
       },
     },
