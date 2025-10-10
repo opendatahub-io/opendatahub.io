321cecec21c9a384fc0feee07d278f178e8e8d64
Saanchi Sampath <ssampath@redhat.com>
2025-10-10 10:10:42 -0400

    2.24 upstream publishing

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -70,7 +70,7 @@ const config: GatsbyConfig = {
       options: {
         name: `docs`,
         remote: `https://github.com/opendatahub-io/opendatahub-documentation.git`,
-        branch: `v2.23`,
+        branch: `v2.24`,
         local: "public/static/docs",
       },
     },
